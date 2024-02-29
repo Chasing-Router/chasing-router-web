ece2b69c5c543265981be436d22a9a394317eb76
Abby Plumley <abbyplumley741@gmail.com>
2024-02-28 21:08:44 -0500

    completed the catalog feature

---
+++ frontend/src/Products/data.tsx
@@ -0,0 +1,20 @@
+export default {
+    products: [{
+        name: 'Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        discount: 0,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },
+    {
+        name: 'Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        discount: 0,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    }]
+}
